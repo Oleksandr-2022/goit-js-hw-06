944b351f3c1d198381c5383e7e15a9011199ab8d
Oleksandr-2022 <zibyuk.2017@gmail.com>
2022-10-15 21:07:01 +0300

    Завдання №3

--- js/task-03.js
+++ js/task-03.js
@@ -15,15 +15,31 @@ const images = [
 
 
 const galleryList = document.querySelector('.gallery');
-
 const element = images.map(({ url, alt }) => {
-  galleryList.insertAdjacentHTML(
-    'beforeend',
-    `<li class = 'gallery__item'><img class = 'gallery__img' src = ${url} alt = ${alt}></li>`
-  );
-  images.join(' ');
-  return images;
-});
+// `<li class = 'gallery__item'><img class = 'gallery__img' src = ${url} alt = ${alt}></li>`).join('');
+return `<li><img style ='border-radius: 12px' src='${url}' alt='${alt}' width = 250 height = 200></li>`
+}).join('');
+galleryList.insertAdjacentHTML('afterbegin', element);
+galleryList.setAttribute('style', 'list-style-type:none; display: flex; justify-content: space-evenly;')
+
+
+// Доброго дня, Олександр) 
+// У третьмоу завданні потрібно використати тільки метод insertAdjacentHTML,  в тому місці, де ви використовуєте метод append. 
+// Метод append не потрібен. 
+// Також потрібно додати return для поверненя розмітки з методу map.
+//  В кінці додати метод join,  щоб перетворити масив на рядок.
+
+
+// const galleryList = document.querySelector('.gallery');
+
+// const element = images.map(({ url, alt }) => {
+//   galleryList.insertAdjacentHTML(
+//     'beforeend',
+//     `<li class = 'gallery__item'><img class = 'gallery__img' src = ${url} alt = ${alt}></li>`
+//   );
+//   images.join('');
+//   return gallery;
+// });
 // galleryList.append(...element);
 
 
